a960240973190593e3991f5bc443d1dbf98ba4ca
bradyvercher <bradyvercher@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-16 00:44:42 +0000

    Release 1.1.1
    git-svn-id: https://plugins.svn.wordpress.org/plugin-toggle/tags/1.1.1@858673 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ plugin-toggle.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Plugin Toggle
+ *
+ * @package PluginToggle
+ * @author Brady Vercher
+ * @copyright Copyright (c) 2014, Blazer Six, Inc.
+ * @license GPL-2.0+
+ *
+ * @wordpress-plugin
+ * Plugin Name: Plugin Toggle
+ * Plugin URI: http://www.blazersix.com/
+ * Description: Quickly toggle plugin activation status from the toolbar.
+ * Version: 1.1.1
+ * Author: Blazer Six
+ * Author URI: http://www.blazersix.com/
+ * License: GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+/**
+ * Plugin directory URL.
+ */
+if ( ! defined( 'PLUGINTOGGLE_URL' ) ) {
+	define( 'PLUGINTOGGLE_URL', plugin_dir_url( __FILE__ ) );
+}
+
+/**
+ * Plugin directory path.
+ */
+if ( ! defined( 'PLUGINTOGGLE_DIR' ) ) {
+	define( 'PLUGINTOGGLE_DIR', plugin_dir_path( __FILE__ ) );
+}
+
+/**
+ * Include classes.
+ */
+require( PLUGINTOGGLE_DIR . 'includes/class-plugintoggle.php' );
+require( PLUGINTOGGLE_DIR . 'includes/class-plugintoggle-plugin.php' );
+require( PLUGINTOGGLE_DIR . 'includes/class-plugintoggle-toolbar.php' );
+
+$plugintoggle = new PluginToggle();
+
+add_action( 'plugins_loaded', array( $plugintoggle, 'load_plugin' ) );
